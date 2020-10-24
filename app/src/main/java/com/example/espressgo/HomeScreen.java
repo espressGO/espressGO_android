7eee98ef91772361adaded22f43d4fcd232959b7
dmurphgarrett <dmurphgarrett@gmail.com>
2020-10-24 16:03:50 -0400

    Adding nav drawer functionality to local branch

--- app/src/main/java/com/example/espressgo/HomeScreen.java
+++ app/src/main/java/com/example/espressgo/HomeScreen.java
@@ -1,41 +1,39 @@
 package com.example.espressgo;
+
 import android.os.Bundle;
-//import android.graphics.Color;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import models.Users;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class HomeScreen extends AppCompatActivity {
+    private DrawerLayout drawer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-//        tvUsername = (TextView) findViewById(R.id.tvUsername);
-//        tvPassword = (TextView) findViewById(R.id.tvPassword);
-//        edUsername = (EditText) findViewById(R.id.edUsername);
-//        edPassword = (EditText) findViewById(R.id.edPassword);
 
-//        btnLogin = (Button) findViewById(R.id.btnLogin);
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        Toolbar tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tb, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
