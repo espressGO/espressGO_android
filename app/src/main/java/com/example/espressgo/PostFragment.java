7e1a722ff1e128388bdc263a992bb4c82588a97c
dmurphgarrett <dmurphgarrett@gmail.com>
2020-11-02 16:47:53 -0500

    "Adding some xml pages (search, profile)"

--- app/src/main/java/com/example/espressgo/PostFragment.java
+++ app/src/main/java/com/example/espressgo/PostFragment.java
@@ -1,6 +1,7 @@
 package com.example.espressgo;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -52,5 +54,23 @@ public class PostFragment extends Fragment implements View.OnClickListener{
         //etDrink can be empty if it is a shop rating, but if it isn't empty check if it exists
         //Make sure etComment isn't empty
         //Once this is done, create a new post variable and add it to the database!
+        if (TextUtils.isEmpty((CharSequence) etShop)) {
+            Toast.makeText(getActivity(), "Shop is empty. Please re-enter the shop you wish to review.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else if (TextUtils.isEmpty((CharSequence) etComment)) {
+            Toast.makeText(getActivity(), "Comment is empty. Please re-enter the comment you wish to post.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //Shops shop = searchForShop((CharSequence) etShop);
+//        if (TextUtils.isEmpty((CharSequence) etShop)) {
+//            //drink is empty, post the comment here without a drink
+//        }
+//        else {
+//            //Drinks drink = searchForDrink((CharSequence) etDrink)
+//            //make post here with a drink
+//        }
+
     }
 }
